549083058f3065501d8b62583b8a7ba534b7a4b7
haeuniea <haeunhaa@gmail.com>
2026-08-14 02:07:14 +0900

    feat: Todo 엔티티에 JPA 매핑 적용

--- src/main/java/com/asdf/todo/entity/Todo.java
+++ src/main/java/com/asdf/todo/entity/Todo.java
@@ -1,17 +1,36 @@
 package com.asdf.todo.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-// 모델 클래스
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NonNull private String title;
+
+    @NonNull
+    @Column(nullable = false, name = "title")
+    private String title;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(nullable = false, name = "completed")
     private boolean completed;
+
+    @Column(nullable = false, name = "created_at",
+    insertable = false, updatable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
 }
