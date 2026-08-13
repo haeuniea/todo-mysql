303fd21a7cf7ff46cd99585b5638d3e47ccfc331
haeuniea <haeunhaa@gmail.com>
2026-08-14 01:49:06 +0900

    refactor: 메인 클래스명을 TodoApplication으로 변경

--- src/main/java/com/asdf/todo/TodoApplication.java
+++ src/main/java/com/asdf/todo/TodoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoInMemoryApplication {
+public class TodoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodoInMemoryApplication.class, args);
+        SpringApplication.run(TodoApplication.class, args);
     }
 }
